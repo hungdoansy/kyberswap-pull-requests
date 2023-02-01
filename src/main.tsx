4ff97b990fd51a9022cfc4f9afadd2be82af805a
Doan Sy Hung <hungdoansy@gmail.com>
2023-02-01 15:44:07 +0700

    feat: detect mobile browsers

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
 import App from "./App"
 import "./index.css"
+import DetectMobileBrowser from "./pages/DetectMobileBrowsers"
 import QR from "./pages/QR"
 import RedirectTo from "./pages/RedirectTo"
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/qr" element={<QR />} />
+                <Route path="/br" element={<DetectMobileBrowser />} />
                 <Route path=":prID" element={<RedirectTo toPreview />} />
                 <Route path="/pr/:prID" element={<RedirectTo />} />
                 <Route path="*" element={<Navigate replace to="/" />} />
